3cd76738368759f60858bc1640b0199b5a011413
IHateNamo <thamatijaru@gmail.com>
2025-03-18 15:01:28 +0700

    4.3

---
+++ cell04/ex03/float.py
@@ -0,0 +1,10 @@
+num_input=input("gimme a number: ")
+
+try:
+    num = float(num_input)
+    if num == int(num):
+        print("Thiis number is interger")
+    else:
+        print("This number is decimal")
+except ValueError:
+    print("Invaid input try again")
