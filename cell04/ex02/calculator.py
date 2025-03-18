9445b97cb62905383da02b22151ccff330f0440a
IHateNamo <thamatijaru@gmail.com>
2025-03-18 14:21:39 +0700

    4.2

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+first_num =int(input("Gimme the first number : "))
+sec_num =int(input("Gimme the second number : ")) 
+
+plus_result = first_num + sec_num
+minus_result = first_num - sec_num
+multi_result = first_num * sec_num
+divine_result = first_num / sec_num
+
+print("Thank you! ")
+print(f"{first_num} + {sec_num} = {plus_result}")
+print(f"{first_num} - {sec_num} = {minus_result}")
+print(f"{first_num} / {sec_num} = {divine_result}")
+print(f"{first_num} * {sec_num} = {multi_result}")
