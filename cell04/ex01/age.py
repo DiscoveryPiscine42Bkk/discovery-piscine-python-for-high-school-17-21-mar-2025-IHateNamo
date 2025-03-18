0f7c7cc8d4497a76b7b8950499cb60fa018906f1
IHateNamo <thamatijaru@gmail.com>
2025-03-18 13:42:57 +0700

    4.1

---
+++ cell04/ex01/age.py
@@ -0,0 +1,9 @@
+age_now = (int(input("Please tell me your age")))
+
+a = age_now + 10
+b = age_now + 20
+c = age_now + 30
+
+print("in 10 year, you'll be", a ,"year old" )
+print("in 20 year, you'll be", b ,"year old" )
+print("in 30 year, you'll be", c ,"year old" )
