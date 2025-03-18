002e24596bb0dc15c9390a482a74d886b58d57c0
IHateNamo <thamatijaru@gmail.com>
2025-03-18 09:59:32 +0700

    2.3

---
+++ cell02/ex03/muti.py
@@ -0,0 +1,14 @@
+first_num = int(input('Enter the first number: '))
+sec_num = int(input('Enter the second number: '))
+Multi_kan = first_num * sec_num
+
+print(str(first_num) + " x " + str(sec_num) + " = " , Multi_kan)
+
+if Multi_kan > 0:
+    print("The result is positive. ")
+elif Multi_kan < 0:
+    print("The result is negative. ")
+elif Multi_kan == 0:
+    print("The result is positive and negative. ")
+else:
+    print("ERROR Enter number again .")
