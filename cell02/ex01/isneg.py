a7a980d63b728e494f20a4bbeb8b511ca01fd6b3
IHateNamo <thamatijaru@gmail.com>
2025-03-18 09:09:34 +0700

    2.1 py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,10 @@
+Pos_num = int(input("input your number : "))
+
+if(Pos_num > 0):
+    print("This number is positive")
+elif(Pos_num < 0):
+    print("This number is negative")
+elif(Pos_num == 0):
+    print("This number is both negative and positive")
+else:
+    print("Error pls write again")
