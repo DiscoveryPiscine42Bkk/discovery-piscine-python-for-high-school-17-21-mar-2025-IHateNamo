5980a81f3062223022ba0ed79a8985806f17409b
IHateNamo <thamatijaru@gmail.com>
2025-03-19 13:10:21 +0700

    5.5-5.7

---
+++ cell05/ex06/uppcase_it.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) !=2:
+    print("none")
+else:
+    input_str = sys.argv[1]
+    uppercase_str = input_str.upper()
+    print(uppercase_str)
+
+
---
+++ cell05/ex07/lowercase_it.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) !=2:
+    print("none")
+else:
+    input_str = sys.argv[1]
+    lowercase_str = input_str.lower()
+    print(lowercase_str)
+
+
