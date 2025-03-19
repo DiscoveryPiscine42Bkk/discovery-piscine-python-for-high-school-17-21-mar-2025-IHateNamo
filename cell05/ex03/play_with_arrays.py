61a0f695fabe8f597e6c60859112d3751f6d5582
IHateNamo <thamatijaru@gmail.com>
2025-03-19 11:07:13 +0700

    wtf array

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+origin_array = [2,8,9,48,8,22,-12,2]
+new_array = [i+2 for i in origin_array]
+greater_than = {i for i in new_array if i>5}
+
+print(origin_array)
+print(greater_than)
