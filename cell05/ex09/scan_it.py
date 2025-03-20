e3cb26b13a9f8aa2506ff17c4834381afaf44bbd
IHateNamo <thamatijaru@gmail.com>
2025-03-20 13:40:01 +0700

    correct 5.9

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -4,6 +4,5 @@ import re
 
 if len(sys.argv) != 3:
     print("none")
-
 else:
-    print(len(re.findall(syssys.argv[2]))).argv[1], 
+    print(len(re.findall(sys.argv[1], sys.argv[2])))  # Corrected the call to re.findall
